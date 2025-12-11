a8cc6bc3f638562e0643d3bfa68a6273bfd65fd9
Caio Santos <caiosantos.dev@outlook.com>
2025-12-11 07:44:50 -0300

    feat: add

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -3,6 +3,7 @@
 import { userLoggerAtom } from "@/jotai/auth/auth.jotai";
 import { useAtom } from "jotai";
 import { Autorization } from "@/components/Global/Autorization";
+import { FormResetPassword } from "@/components/Auth/ResetPassword";
 
 export default function ResetPassword() {
   const [userLogger] = useAtom(userLoggerAtom);
@@ -14,7 +15,7 @@ export default function ResetPassword() {
         !userLogger &&
         <main className="h-dvh slim-bg-secondary">
           <div className="w-11/12 h-12/12 lg:max-w-md m-auto flex flex-col justify-center">
-            <ResetPassword />
+            <FormResetPassword />
           </div>
         </main>
       }
