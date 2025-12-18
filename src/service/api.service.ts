d0bd7488f3ff1efa4cca389171bb047550f91667
Caio Santos <caiosantos.dev@outlook.com>
2025-12-18 08:49:36 -0300

    coommit

--- src/service/api.service.ts
+++ src/service/api.service.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-
-export const baseURL = process.env.NEXT_PUBLIC_API_URL;
+// export const uriBase = 'http://localhost:5103'
+export const uriBase = 'http://72.62.11.203:3001'
+export const baseURL = `${uriBase}/api`;
 
 export const api = axios.create({
-  // baseURL: 'http://localhost:5103/api',
-  baseURL: 'http://72.62.11.203:3001/api',
+  baseURL
 });
