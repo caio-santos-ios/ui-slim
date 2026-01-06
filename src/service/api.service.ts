c3998ef5d4fbf968dc179e012c6f11ac811d3301
Caio Santos <caiosantos.dev@outlook.com>
2026-01-06 10:08:58 -0300

    fix

--- src/service/api.service.ts
+++ src/service/api.service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // export const uriBase = 'http://localhost:5103'
-export const uriBase = 'http://72.62.11.203:3001'
+export const uriBase = 'https://api.pasbem.com.br'
 export const baseURL = `${uriBase}/api`;
 
 export const api = axios.create({
