3a5c90366310114010586c9b96d73c93cfa9f808
Caio Santos <caiosantos.dev@outlook.com>
2026-03-31 14:25:24 -0300

    fix

--- src/app/settings/permission-profiles/layout.tsx
+++ src/app/settings/permission-profiles/layout.tsx
@@ -6,7 +6,7 @@ const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "SLIM | Perfis de Permissão",
+  title: "Pasbem | Perfis de Permissão",
   description: "Configuração de perfis de permissão",
 };
 
